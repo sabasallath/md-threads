e0bb149ec0eb2bc25851f0070d4b6e959751b4bb
Daniel Blévin <dan.le.blev@gmail.com>
2021-06-14 21:51:51 +0200

    Remove base href confusion

--- src/config/path.ts
+++ src/config/path.ts
@@ -1,5 +1,5 @@
 export const href = (): string => {
-  return ((window as unknown) as Record<string, string>).BASE_HREF;
+  return process.env.REACT_APP_BASE_HREF || '/';
 };
 
 export const i18nJsonUrl = (): string => {
